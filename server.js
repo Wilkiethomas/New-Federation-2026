c104cc4fbab022ffbe7fc47c1c0b5dacc05c47f4
Wilkiethomas <wilkiethomas6@gmail.com>
2026-02-05 14:41:10 -0500

    Fix dotenv path

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
  * Serves both API and Frontend
  */
 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
